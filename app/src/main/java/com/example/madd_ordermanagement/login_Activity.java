0319b5269715e41f4079c2ac18986ce73f090d8a
TharinduMaheesha <it19162706@my.sliit.lk>
2020-08-15 15:54:41 +0530

    Login Activity Created

---
+++ app/src/main/java/com/example/madd_ordermanagement/login_Activity.java
@@ -0,0 +1,26 @@
+package com.example.madd_ordermanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+
+        Button btn1 = findViewById(R.id.btnSignIn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login_Activity.this,CustomerProductHome_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
