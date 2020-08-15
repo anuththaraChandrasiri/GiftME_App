c7147b91b2da230250cf7298684da8ba970f1588
Thathsara Hewage <67362351+ThathsaraHewage@users.noreply.github.com>
2020-08-15 22:23:21 +0530

    DeliveryM_ordered by page created

---
+++ app/src/main/java/com/example/madd_Deliverymanagement/OrderTo.java
@@ -0,0 +1,20 @@
+package com.example.madd_Deliverymanagement;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.madd_ordermanagement.R;
+
+public class OrderTo extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+}
