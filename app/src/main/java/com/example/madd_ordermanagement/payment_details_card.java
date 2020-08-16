aef29350d167685a56f641b844587b17165bae99
Duvindu-Pc <maxduvindu1999@gmail.com>
2020-08-16 12:02:47 +0530

    aaa

---
+++ app/src/main/java/com/example/madd_ordermanagement/payment_details_card.java
@@ -0,0 +1,14 @@
+package com.example.madd_ordermanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class payment_details_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_details_card);
+    }
+}
